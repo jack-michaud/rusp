16108bd9b7dc2bd8a372d1a20c31e7caaee12145
jack-michaud <mrmichaud42@gmail.com>
2017-07-27 09:28:25 -0400

    Move to cargo setup, get tokenizing

---
+++ rusp/expressions.rs
@@ -0,0 +1,51 @@
+use std::fmt;
+
+
+pub enum Argument {
+    Expression {
+	    func:   String,
+	    args: Box<Vec<Argument>>,
+	},
+    Atom {
+        content: Option<i32>
+    }
+}
+
+
+impl Argument {
+
+    pub fn new(func: String, args: Vec<Argument>) -> Argument {
+        Argument::Expression { func: func, args: Box::new(args) }
+    }
+
+    pub fn stringify(&self) -> String {
+    	match *self {
+    		Argument::Expression { ref args, ref func } => {
+    			println!("We got an expression");
+    			let mut to_return = String::from("(");
+                to_return.push_str(func.as_str());
+                to_return.push_str(" ");
+                for arg in args.iter() {
+                    to_return.push_str(&arg.stringify().as_str());
+                    to_return.push_str(" ");
+                }
+                to_return.push_str(")");
+                to_return
+    		},
+    		Argument::Atom { ref content } => {
+    			println!("We got an Atom");
+                match *content {
+                    Some(ref i) => {
+                        let mut to_return = String::from("");
+                        to_return.push_str(i.to_string().as_str());
+                        to_return
+                    },
+                    None => String::from("")
+                }
+                
+    		}
+
+    	}
+    }
+}
+
---
+++ rusp/main.rs
@@ -0,0 +1,25 @@
+use std::env;
+
+use std::fs::File;
+
+mod parser;
+mod expressions;
+// const foo: str = include_str!("test.lisp");
+
+fn main() {
+	
+	let cl_args = env::args();
+	if cl_args.count() < 2 {
+		println!("Must provide a rusp file. Usage: ./cargo run <example.lisp>");
+		return;
+	}
+
+
+	let file = match File::open("test.lisp") {
+		Ok(v) => v,
+		Err(_) => panic!("Couldnt open rusp file.")
+	};
+	let my_exp = parser::parse_file(file);
+	
+    println!("{:?}", my_exp.stringify());
+}
